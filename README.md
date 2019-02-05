5a88721d57faf3ca39e3d3d2b61ee953bfa87ff7
Brian Perrett <perrettbrian@gmail.com>
2019-02-05 10:24:13 -0800

    move setup.py and add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## League of Legends API Wrapper
+
+Uses V4 where possible.
+
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 
 setuptools.setup(
     name='lol',
-    version='0.01',
-    packages=['resource', ],
+    version='0.1dev',
+    packages=['lol', ],
     author='brianperrett',
     author_email='perrettbrian@gmail.com',
     description='riot api wrapper',
