cc145bc4875e208a59711052773a1fa3c0cd3356
Brian Perrett <perrettbrian@gmail.com>
2019-06-07 13:57:05 -0700

    change setup again

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 import setuptools
+from os import path
 
+
+BASE = path.abspath(path.dirname(__file__))
+
+with open(path.join(BASE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='lolwrapper',
@@ -8,7 +14,7 @@ setuptools.setup(
     author='Brian Perrett',
     author_email='perrettbrian@gmail.com',
     description='Riot API Wrapper',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/brianjp93/lolapi",
     classifiers=[
