3b4eb4f937471602bc73d53c9e51fa4074e6cbfb
Brian Perrett <perrettbrian@gmail.com>
2019-06-07 13:31:26 -0700

    ignore pypi build and dist folders

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import setuptools
 
 
 setuptools.setup(
-    name='lol',
+    name='lolapi',
     version='1.0',
-    packages=['lol', ],
-    author='brianperrett',
+    packages=setuptools.find_packages(),
+    author='Brian Perrett',
     author_email='perrettbrian@gmail.com',
     description='Riot API Wrapper',
-    long_description=open('README.md').read(), 
+    long_description=open('README.md').read(),
+    url="https://github.com/brianjp93/lolapi",
 )
