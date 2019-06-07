10f74ef36f541bd31ca831593c9cbaf03ec4b4a5
Brian Perrett <perrettbrian@gmail.com>
2019-06-07 13:49:35 -0700

    edit setup.py

--- setup.py
+++ setup.py
@@ -11,4 +11,9 @@ setuptools.setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/brianjp93/lolapi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
