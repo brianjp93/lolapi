bff533ab9a3a1c87ab2e792e13bb4f4b66272c26
Brian Perrett <perrettbrian@gmail.com>
2023-11-22 01:45:29 -0800

    add get-by-puuid

--- lol/resource/account.py
+++ lol/resource/account.py
@@ -16,3 +16,9 @@ class Account:
         url = '{}/riot/account/v1/accounts/by-riot-id/{}/{}'.format(base_url, game_name, tag_line)
         r = requests.get(url, headers=self.base.headers)
         return r
+
+    def by_puuid(self, puuid, region='americas'):
+        base_url = self.base.get_base_url(region, use_v5_region=True)
+        url = '{}/riot/account/v1/accounts/by-puuid/{}'.format(base_url, puuid)
+        r = requests.get(url, headers=self.base.headers)
+        return r
